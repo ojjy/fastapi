07322668eace3b62e7a63f4584c86c845b544e0c
yejinjo <yejinjo@gccorp.com>
2024-05-08 14:26:55 +0900

    file upload

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from board import board_router
 from user import user_router
 from contain import contain_router
 import boto3
-
+import os
 # from common import key
 app = FastAPI()
 
@@ -38,15 +38,21 @@ async def upload_file(request: Request):
 
 @app.post("/file/store", response_class=HTMLResponse)
 async def store_file(request: Request, file: UploadFile=File(...)):
-
     try:
+        upload_folder_fullpath = os.path.join(os.path.abspath(os.path.dirname(file.filename)), "uploads")
+        print(upload_folder_fullpath)
+        upload_file_fullpath = os.path.join(upload_folder_fullpath, "uploaded_" + file.filename)
+        print(upload_file_fullpath)
+
         contents = file.file.read()
-        with open("uploaded_" + file.filename, "wb") as f:
+        with open(upload_file_fullpath, "wb") as f:
             f.write(contents)
     except Exception:
         return {"message": "There was an error uploading the file"}
     finally:
+
         file.file.close()
+        return templates.TemplateResponse("display.html", {"request": request,  "filename": {file.filename}})
 
 if __name__ == "__main__":
     uvicorn.run('main:app',reload=True, host='0.0.0.0', port=8080)
